861281fa4ce7bb31cdfd2a073cef06d95ef092ba
Serdar <serdar2225@gmail.com>
2020-08-08 08:58:41 -0400

    Initial commit

---
+++ src/test/java/CountryTest.java
@@ -0,0 +1,12 @@
+import io.restassured.http.ContentType;
+
+import org.testng.annotations.Test;
+
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class CountryTest {
+    
+}
